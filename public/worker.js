fa454377c3c3eee8524a7785980470e01ed5eeba
Amith Jayapraban <amithjayapraban@Amiths-MacBook-Air.local>
2023-02-13 19:11:03 +0530

    fi

--- public/worker.js
+++ public/worker.js
@@ -1,7 +1,7 @@
 onmessage = (e) => {
- var file = e.data.fl;
-for(let f=0;f<file.length;f++){
-  file[f].arrayBuffer().then((buffer) => {
+  var file = e.data.file;
+
+  file.arrayBuffer().then((buffer) => {
     const chunkSize = 16 * 1024;
     let total_len = buffer.byteLength;
     while (buffer.byteLength) {
@@ -14,23 +14,4 @@ for(let f=0;f<file.length;f++){
     }
     postMessage("completed");
   });
-
-}
 };
-
-// file.arrayBuffer().then((buffer: any) => {
-// const chunkSize = 16 * 1024;
-// let total_len = buffer.byteLength;
-// var prog1: any = document.querySelector(".progress");
-// prog1.classList.remove("w-0");
-// var prog: any = document.getElementById("progress");
-// while (buffer.byteLength) {
-//   const chunk = buffer.slice(0, chunkSize);
-//   buffer = buffer.slice(chunkSize, buffer.byteLength);
-//   dataChannel.send(chunk);
-
-//   prog.style.opacity = "1";
-//   let w = (buffer.byteLength / total_len) * 100 - 100;
-//   prog.style.width = `${Math.abs(w * 2)}%`;
-//   console.log(Math.abs(w));
-// }
